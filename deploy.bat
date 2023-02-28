05173ca0f0388f107d635a0f6ab9d16e17bb94e3
Robin Jones <networkfusion@users.noreply.github.com>
2023-02-28 21:03:14 +0000

    Improve deployment text

--- deploy.bat
+++ deploy.bat
@@ -1,8 +1,21 @@
-:: Make sure we are connected
+@echo off
+
 cd  %~dp0tools\sc64\
+
+:: Make sure we are connected
+echo Detecting SC64...
 sc64 --print-state
 
-:: Boot the menu
+echo:
+echo:
+:: Load the ROM
+echo Loading ROM...
 sc64 --boot direct-rom %~dp0build\N64FlashcartMenu.z64
 
+echo:
+echo:
+:: Toggle the power of the N64 to boot the ROM.
+echo !!! Now toggle power to the N64 !!!
+echo:
+echo:
 ::pause

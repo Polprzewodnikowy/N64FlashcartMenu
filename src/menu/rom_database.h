f8fb8b617e0e0754a55598680bf05bb39c0e6c0a
Robin Jones <networkfusion@users.noreply.github.com>
2023-07-16 11:27:56 +0100

    ROM DB additions (#12)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Description
    <!--- Describe your changes in detail -->
    Add some missing cart ID's to the database
    
    ## Motivation and Context
    <!--- What does this sample do? What problem does it solve? -->
    <!--- If it fixes/closes/resolves an open issue, please link to the
    issue here -->
    Some were missing.
    
    ## How Has This Been Tested?
    <!-- (if applicable) -->
    <!--- Please describe in detail how you tested your sample/changes. -->
    <!--- Include details of your testing environment, and the tests you ran
    to -->
    <!--- see how your change affects other areas of the code, etc. -->
    
    ## Screenshots
    <!-- (if appropriate): -->
    
    ## Types of changes
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    - [x] Improvement (non-breaking change that adds a new feature)
    - [ ] Bug fix (fixes an issue)
    - [ ] Breaking change (breaking change)
    - [ ] Config and build (change in the configuration and build system,
    has no impact on code or features)
    
    ## Checklist:
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    - [x] My code follows the code style of this project.
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    - [ ] I have added tests to cover my changes.
    - [ ] All new and existing tests passed.
    
    <!--- It would be nice if you could sign off your contribution by
    replacing the name with your GitHub user name and GitHub email contact.
    -->
    Signed-off-by: GITHUB_USER <GITHUB_USER_EMAIL>
    
    ---------
    
    Co-authored-by: Mateusz Faderewski <sc@mateuszfaderewski.pl>

--- src/menu/rom_database.h
+++ src/menu/rom_database.h
@@ -12,27 +12,29 @@
 
 // NOTE: these values are independent of flashcart / OS
 // But by default align to SC64.
-#define DB_SAVE_TYPE_NONE           0x00
-#define DB_SAVE_TYPE_EEPROM_4K      0x01
-#define DB_SAVE_TYPE_EEPROM_16K     0x02
-#define DB_SAVE_TYPE_SRAM           0x03
-#define DB_SAVE_TYPE_SRAM_BANKED    0x04
-#define DB_SAVE_TYPE_SRAM_128K      0x05
-#define DB_SAVE_TYPE_FLASHRAM       0x06
-#define DB_SAVE_TYPE_CPAK           0x10
-#define DB_SAVE_TYPE_DD             0x20
-#define DB_SAVE_TYPE_INVALID        0xff
+typedef enum {
+    DB_SAVE_TYPE_NONE =          0x00,
+    DB_SAVE_TYPE_EEPROM_4K =     0x01,
+    DB_SAVE_TYPE_EEPROM_16K =    0x02,
+    DB_SAVE_TYPE_SRAM =          0x03,
+    DB_SAVE_TYPE_SRAM_BANKED =   0x04,
+    DB_SAVE_TYPE_SRAM_128K =     0x05,
+    DB_SAVE_TYPE_FLASHRAM =      0x06,
+    DB_SAVE_TYPE_CPAK =          0x10,
+    DB_SAVE_TYPE_DD =            0x20,
+    DB_SAVE_TYPE_INVALID =       0xff,
+ } db_savetype_t;
 
 /** @brief ROM System Memory requirements enumeration */
 typedef enum {
-    /** @brief The ROM is happy with 4K of memory */
-    DB_MEMORY_EXPANSION_NONE =      0x00,
-    /** @brief The ROM requires 8K of memory */
-    DB_MEMORY_EXPANSION_REQUIRED =  0x01,
-    DB_MEMORY_EXPANSION_SUGGESTED = 0x02,
-    DB_MEMORY_EXPANSION_ENHANCED =  0x03,
-    /** @brief The ROM is faulty when using 8K of memory */
-    DB_MEMORY_EXPANSION_FAULTY =    0x04,
+    /** @brief The ROM is happy with 4MB of memory */
+    DB_MEMORY_EXPANSION_NONE =        0x00,
+    /** @brief The ROM requires 8MB of memory */
+    DB_MEMORY_EXPANSION_REQUIRED =    0x01,
+    DB_MEMORY_EXPANSION_RECOMMENDED = 0x02,
+    DB_MEMORY_EXPANSION_SUGGESTED =   0x03,
+    /** @brief The ROM is faulty when using 8MB of memory */
+    DB_MEMORY_EXPANSION_FAULTY =      0x04,
 } rom_memorytype_t;
 
 
@@ -83,7 +85,7 @@ typedef enum {
     // MARKET_UNKNOWN_M = 'M',
     MARKET_CANADA = 'N',
     // MARKET_UNKNOWN_O = 'O',
-    MARKET_EUROPE_P = 'P',
+    MARKET_PAL_GENERIC = 'P',
     // MARKET_UNKNOWN_Q = 'Q',
     // MARKET_UNKNOWN_R = 'R',
     MARKET_SPAIN = 'S',
@@ -91,9 +93,9 @@ typedef enum {
     MARKET_AUSTRAILA = 'U',
     // MARKET_UNKNOWN_V = 'V',
     MARKET_SCANDINAVAIA = 'W',
-    MARKET_EUROPE_X = 'X',
-    MARKET_EUROPE_Y = 'Y',
-    MARKET_EUROPE_Z = 'Z'
+    MARKET_PAL_X = 'X',
+    MARKET_PAL_Y = 'Y',
+    MARKET_PAL_Z = 'Z'
 } rom_destination_market_t;
 
 

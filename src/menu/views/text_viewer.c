b71dc18ad673d1515ad19f85a1085c50f0239bdc
Robin Jones <networkfusion@users.noreply.github.com>
2023-08-05 16:03:35 +0100

    Improve file_viewer implementation

--- src/menu/views/text_viewer.c
+++ src/menu/views/text_viewer.c
@@ -2,11 +2,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#include "fragments/fragments.h"
 #include "views.h"
 
-static char *file_line_content[20];
-static uint16_t file_line_count = 0;
+static char *file_content;
 
 static void process (menu_t *menu) {
     if (menu->actions.back) {
@@ -16,45 +14,39 @@ static void process (menu_t *menu) {
 
 static void draw (menu_t *menu, surface_t *d) {
     
-    const color_t text_color = RGBA32(0x00, 0x00, 0x00, 0xFF);
-    const color_t bg_color = RGBA32(0xFF, 0xFF, 0xFF, 0xFF);
-
     rdpq_attach(d, NULL);
-    rdpq_clear(bg_color);
-
-    fragment_text_start(text_color);
 
     //display file content.
+    component_background_draw();
 
-    const int offset_x = 40;
-    int offset_y = 40;
+    component_main_text_draw(
+        ALIGN_CENTER, VALIGN_TOP,
+        "FILE CONTENT"
+    );
 
-    rdpq_font_begin(text_color);
-    
-    for (int i = 0; i < file_line_count - 1; i++)
-    {
-        if (file_line_content[i] != NULL) {
-            offset_y += fragment_textf(offset_x, offset_y, file_line_content[i]);
-        }
-    }
+    // FIXME: should have scrollbars if required.
+    component_main_text_draw(
+        ALIGN_LEFT, VALIGN_TOP,
+        "\n\n%s",
+        file_content
+    );
 
-    fragment_textf(offset_x, d->height - 40, "B: Exit");
+    component_actions_bar_text_draw(
+        ALIGN_LEFT, VALIGN_TOP,
+        "B: Exit"
+    );
+
     rdpq_detach_show();
 }
 
 
 static void deinit (menu_t *menu) {
     // free memory
-    for (int i = 0; i < file_line_count; i++)
-    {
-        free(file_line_content[i]);
-    }
+    free(file_content);
 }
 
 
 void view_text_viewer_init (menu_t *menu) {
-
-    file_line_count = 0;
 
     path_t *path = path_clone(menu->browser.directory);
     path_push(path, menu->browser.list[menu->browser.selected].name);
@@ -63,6 +55,10 @@ void view_text_viewer_init (menu_t *menu) {
     char *sd_path = calloc(4 + strlen(path_get(path)) + 1, sizeof(char));
     sprintf(sd_path, "sd:/%s", path_get(path));
 
+    uint16_t file_size = 2048;  // FIXME: get proper file size and set a reasonable maximum.
+
+    file_content = calloc(file_size, sizeof(char));
+
     FILE *fp = fopen(sd_path, "r");
 
     debugf("loading path: %s\n", sd_path);
@@ -70,20 +66,7 @@ void view_text_viewer_init (menu_t *menu) {
         debugf("Error loading file content\n");
     }
     else {
-        // read the first 20 lines of the file with a max line length of 256 chars
-        for (int i = 0; i < 20; i++)
-        {
-            if( feof(fp) ) {
-                break;
-            }
-
-            file_line_content[i] = calloc(256, sizeof(char));
-            fgets(file_line_content[i], 256, fp);
-
-            file_line_count += 1;
-
-        }
-
+        fread(file_content, file_size, sizeof(char), fp);
         fclose(fp);
     }
 

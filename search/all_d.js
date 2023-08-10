00f904c66ccc48a47d373f781679bd51ee68397c
Polprzewodnikowy <Polprzewodnikowy@users.noreply.github.com>
2023-08-10 19:20:33 +0000

    deploy: ac63083089e8a6e055786a2c945b626b656fcb6b

--- search/all_d.js
+++ search/all_d.js
@@ -1,13 +1,15 @@
 var searchData=
 [
   ['sc64_0',['Sc64',['../group__sc64.html',1,'']]],
-  ['sc64_5fbuffers_5ft_1',['sc64_buffers_t',['../group__sc64.html#structsc64__buffers__t',1,'']]],
-  ['sc64_5fcmd_5ft_2',['sc64_cmd_t',['../structsc64__cmd__t.html',1,'']]],
-  ['sc64_5fregs_5ft_3',['sc64_regs_t',['../structsc64__regs__t.html',1,'']]],
-  ['sdk_5fversion_4',['sdk_version',['../rom__database_8h.html#a0115fe44deda826f21523f83edefd517',1,'rom_header_t']]],
-  ['settings_2eh_5',['settings.h',['../settings_8h.html',1,'']]],
-  ['settings_5ft_6',['settings_t',['../settings_8h.html#structsettings__t',1,'']]],
-  ['show_5fhidden_5ffiles_7',['show_hidden_files',['../settings_8h.html#a7f6db7abffab57f3879660afc7187108',1,'settings_t']]],
-  ['sp_5fmem_5ft_8',['sp_mem_t',['../boot__io_8h.html#structsp__mem__t',1,'']]],
-  ['sp_5fregs_5ft_9',['sp_regs_t',['../boot__io_8h.html#structsp__regs__t',1,'']]]
+  ['sc64_2eh_1',['sc64.h',['../sc64_8h.html',1,'']]],
+  ['sc64_5fbuffers_5ft_2',['sc64_buffers_t',['../group__sc64.html#structsc64__buffers__t',1,'']]],
+  ['sc64_5fcmd_5ft_3',['sc64_cmd_t',['../structsc64__cmd__t.html',1,'']]],
+  ['sc64_5fll_2eh_4',['sc64_ll.h',['../sc64__ll_8h.html',1,'']]],
+  ['sc64_5fregs_5ft_5',['sc64_regs_t',['../structsc64__regs__t.html',1,'']]],
+  ['sdk_5fversion_6',['sdk_version',['../rom__database_8h.html#a0115fe44deda826f21523f83edefd517',1,'rom_header_t']]],
+  ['settings_2eh_7',['settings.h',['../settings_8h.html',1,'']]],
+  ['settings_5ft_8',['settings_t',['../settings_8h.html#structsettings__t',1,'']]],
+  ['show_5fhidden_5ffiles_9',['show_hidden_files',['../settings_8h.html#a7f6db7abffab57f3879660afc7187108',1,'settings_t']]],
+  ['sp_5fmem_5ft_10',['sp_mem_t',['../boot__io_8h.html#structsp__mem__t',1,'']]],
+  ['sp_5fregs_5ft_11',['sp_regs_t',['../boot__io_8h.html#structsp__regs__t',1,'']]]
 ];
--- search/files_7.js
+++ search/files_7.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['settings_2eh_0',['settings.h',['../settings_8h.html',1,'']]]
+  ['sc64_2eh_0',['sc64.h',['../sc64_8h.html',1,'']]],
+  ['sc64_5fll_2eh_1',['sc64_ll.h',['../sc64__ll_8h.html',1,'']]],
+  ['settings_2eh_2',['settings.h',['../settings_8h.html',1,'']]]
 ];

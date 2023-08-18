4ea60f9e5644d6b1dc83a34e4935e947fea2f96c
Polprzewodnikowy <Polprzewodnikowy@users.noreply.github.com>
2023-08-18 20:19:40 +0000

    deploy: a1d20ae36ec0e4a7cd1bcf0accd4522db6229b9c

--- search/all_2.js
+++ search/all_2.js
@@ -1,12 +1,13 @@
 var searchData=
 [
   ['cache_5fmetadata_5ft_0',['cache_metadata_t',['../structcache__metadata__t.html',1,'']]],
-  ['checksum_1',['checksum',['../rom__database_8h.html#ade2251d6099a8fb44460f206d79faf62',1,'rom_header_t']]],
-  ['clock_5frate_2',['clock_rate',['../rom__database_8h.html#ae541531dc9e7ae139f3c88716ab303c0',1,'rom_header_t']]],
-  ['component_5fbackground_5ft_3',['component_background_t',['../structcomponent__background__t.html',1,'']]],
-  ['component_5fboxart_5ft_4',['component_boxart_t',['../components_8h.html#structcomponent__boxart__t',1,'']]],
-  ['components_2eh_5',['components.h',['../components_8h.html',1,'']]],
-  ['config_5fflags_6',['config_flags',['../rom__database_8h.html#a36b8274c051911b8739c732d4290d17c',1,'rom_header_t']]],
-  ['constants_2eh_7',['constants.h',['../constants_8h.html',1,'']]],
-  ['crc32_2eh_8',['crc32.h',['../crc32_8h.html',1,'']]]
+  ['cart_5fload_2eh_1',['cart_load.h',['../cart__load_8h.html',1,'']]],
+  ['checksum_2',['checksum',['../rom__database_8h.html#ade2251d6099a8fb44460f206d79faf62',1,'rom_header_t']]],
+  ['clock_5frate_3',['clock_rate',['../rom__database_8h.html#ae541531dc9e7ae139f3c88716ab303c0',1,'rom_header_t']]],
+  ['component_5fbackground_5ft_4',['component_background_t',['../structcomponent__background__t.html',1,'']]],
+  ['component_5fboxart_5ft_5',['component_boxart_t',['../components_8h.html#structcomponent__boxart__t',1,'']]],
+  ['components_2eh_6',['components.h',['../components_8h.html',1,'']]],
+  ['config_5fflags_7',['config_flags',['../rom__database_8h.html#a36b8274c051911b8739c732d4290d17c',1,'rom_header_t']]],
+  ['constants_2eh_8',['constants.h',['../constants_8h.html',1,'']]],
+  ['crc32_2eh_9',['crc32.h',['../crc32_8h.html',1,'']]]
 ];
--- search/files_2.js
+++ search/files_2.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['components_2eh_0',['components.h',['../components_8h.html',1,'']]],
-  ['constants_2eh_1',['constants.h',['../constants_8h.html',1,'']]],
-  ['crc32_2eh_2',['crc32.h',['../crc32_8h.html',1,'']]]
+  ['cart_5fload_2eh_0',['cart_load.h',['../cart__load_8h.html',1,'']]],
+  ['components_2eh_1',['components.h',['../components_8h.html',1,'']]],
+  ['constants_2eh_2',['constants.h',['../constants_8h.html',1,'']]],
+  ['crc32_2eh_3',['crc32.h',['../crc32_8h.html',1,'']]]
 ];

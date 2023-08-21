c0850f89b0fe844cafd3c961fc81f77d58b5562d
Robin Jones <networkfusion@users.noreply.github.com>
2023-08-21 14:32:52 +0100

    ED64 can now load ROM's
    Cleanup source.

--- src/flashcart/ed64/ed64.c
+++ src/flashcart/ed64/ed64.c
@@ -13,104 +13,17 @@
 #include "ed64.h"
 
 
-// #define SRAM_FLASHRAM_ADDRESS       (0x08000000)
-// #define ROM_ADDRESS                 (0x10000000)
-
-// #define SUPPORTED_MAJOR_VERSION     (2)
-// #define SUPPORTED_MINOR_VERSION     (12)
-// #define SUPPORTED_REVISION          (0)
-
-
-// static flashcart_error_t load_to_flash (FIL *fil, void *address, size_t size, UINT *br, flashcart_progress_callback_t *progress) {
-//     size_t erase_block_size;
-//     UINT bp;
-
-//     *br = 0;
-
-//     if (ed64_flash_get_erase_block_size(&erase_block_size) != ED64_OK) {
-//         return FLASHCART_ERROR_INT;
-//     }
-
-//     while (size > 0) {
-//         size_t program_size = MIN(size, erase_block_size);
-//         if (ed64_flash_erase_block(address) != ED64_OK) {
-//             return FLASHCART_ERROR_INT;
-//         }
-//         if (f_read(fil, address, program_size, &bp) != FR_OK) {
-//             return FLASHCART_ERROR_LOAD;
-//         }
-//         if (ed64_flash_wait_busy() != ED64_OK) {
-//             return FLASHCART_ERROR_INT;
-//         }
-//         if (progress) {
-//             progress(f_tell(fil) / (float) (f_size(fil)));
-//         }
-//         address += program_size;
-//         size -= program_size;
-//         *br += bp;
-//     }
-
-//     return FLASHCART_OK;
-// }
-
 static flashcart_error_t ed64_init (void) {
-    // uint16_t major;
-    // uint16_t minor;
-    //uint32_t revision;
+
 
     // ed64_bios_unlock_regs();
 
-    ed64_bios_init();
+    // ed64_bios_init();
     // ed64_bios_reset_spx(); // only required if V2+ FW is updating!
 
-    ed64_bios_set_ram_bank(1); // Seemingly bank 0 (for SRAM 128k) is only supported on V3 and it is not the default.
-    ed64_bios_gpio_mode_io();
+    // ed64_bios_set_ram_bank(1); // Seemingly bank 0 (for SRAM 128k) is only supported on V3 and it is not the default.
+    // ed64_bios_gpio_mode_io();
 
-    // if (!ed64_check_presence()) {
-    //     return FLASHCART_ERROR_UNSUPPORTED;
-    // }
-    // if (ed64_get_version(&major, &minor) != ED64_OK) {
-    //     return FLASHCART_ERROR_OUTDATED;
-    // }
-    // if (major != SUPPORTED_MAJOR_VERSION) {
-    //     return FLASHCART_ERROR_OUTDATED;
-    // }
-    // if (minor < SUPPORTED_MINOR_VERSION) {
-    //     return FLASHCART_ERROR_OUTDATED;
-    // }
-
-    // bool writeback_pending;
-    // do {
-    //     if (ed64_writeback_pending(&writeback_pending) != ED64_OK) {
-    //         return FLASHCART_ERROR_INT;
-    //     }
-    // } while (writeback_pending);
-
-    // const struct {
-    //     ed64_cfg_t id;
-    //     uint32_t value;
-    // } default_config[] = {
-    //     { CFG_ID_BOOTLOADER_SWITCH, false },
-    //     { CFG_ID_ROM_WRITE_ENABLE, false },
-    //     { CFG_ID_ROM_SHADOW_ENABLE, false },
-    //     { CFG_ID_DD_MODE, DD_MODE_DISABLED },
-    //     { CFG_ID_ISV_ADDRESS, 0x00000000 },
-    //     { CFG_ID_BOOT_MODE, BOOT_MODE_MENU },
-    //     { CFG_ID_SAVE_TYPE, SAVE_TYPE_NONE },
-    //     { CFG_ID_CIC_SEED, CIC_SEED_AUTO },
-    //     { CFG_ID_TV_TYPE, TV_TYPE_PASSTHROUGH },
-    //     { CFG_ID_DD_SD_ENABLE, false },
-    //     { CFG_ID_DD_DRIVE_TYPE, DRIVE_TYPE_RETAIL },
-    //     { CFG_ID_DD_DISK_STATE, DISK_STATE_EJECTED },
-    //     { CFG_ID_BUTTON_MODE, BUTTON_MODE_NONE },
-    //     { CFG_ID_ROM_EXTENDED_ENABLE, false },
-    // };
-
-    // for (int i = 0; i < sizeof(default_config) / sizeof(default_config[0]); i++) {
-    //     if (ed64_set_config(default_config[i].id, default_config[i].value) != ED64_OK) {
-    //         return FLASHCART_ERROR_INT;
-    //     }
-    // }
 
     return FLASHCART_OK;
 }
@@ -119,8 +32,8 @@ static flashcart_error_t ed64_deinit (void) {
     // // NOTE: Necessary because libcart enables ROM write by default
     // ed64_set_config(CFG_ROM_WRITE_ENABLE, false);
 
-    ed64_bios_gpio_mode_off(); // On V3, this should be ed64_bios_gpio_mode_rtc() if it is required.
-    ed64_bios_lock_regs();
+    // ed64_bios_gpio_mode_off(); // On V3, this should be ed64_bios_gpio_mode_rtc() if it is required.
+    // ed64_bios_lock_regs();
 
     return FLASHCART_OK;
 }
@@ -256,37 +169,37 @@ static flashcart_error_t ed64_load_save (char *save_path) {
 }
 
 static flashcart_error_t ed64_set_save_type (flashcart_save_type_t save_type) {
-    ed64_save_type_t type;
+    // ed64_save_type_t type;
 
-    switch (save_type) {
-        case FLASHCART_SAVE_TYPE_NONE:
-            type = SAVE_TYPE_NONE;
-            break;
-        case FLASHCART_SAVE_TYPE_EEPROM_4K:
-            type = SAVE_TYPE_EEPROM_4K;
-            break;
-        case FLASHCART_SAVE_TYPE_EEPROM_16K:
-            type = SAVE_TYPE_EEPROM_16K;
-            break;
-        case FLASHCART_SAVE_TYPE_SRAM:
-            type = SAVE_TYPE_SRAM;
-            break;
-        case FLASHCART_SAVE_TYPE_SRAM_BANKED:
-        case FLASHCART_SAVE_TYPE_SRAM_128K:
-            type = SAVE_TYPE_SRAM_128K;
-            break;
-        case FLASHCART_SAVE_TYPE_FLASHRAM:
-            type = SAVE_TYPE_FLASHRAM;
-            break;
-        default:
-            return FLASHCART_ERROR_ARGS;
-    }
+    // switch (save_type) {
+    //     case FLASHCART_SAVE_TYPE_NONE:
+    //         type = SAVE_TYPE_NONE;
+    //         break;
+    //     case FLASHCART_SAVE_TYPE_EEPROM_4K:
+    //         type = SAVE_TYPE_EEPROM_4K;
+    //         break;
+    //     case FLASHCART_SAVE_TYPE_EEPROM_16K:
+    //         type = SAVE_TYPE_EEPROM_16K;
+    //         break;
+    //     case FLASHCART_SAVE_TYPE_SRAM:
+    //         type = SAVE_TYPE_SRAM;
+    //         break;
+    //     case FLASHCART_SAVE_TYPE_SRAM_BANKED:
+    //     case FLASHCART_SAVE_TYPE_SRAM_128K:
+    //         type = SAVE_TYPE_SRAM_128K;
+    //         break;
+    //     case FLASHCART_SAVE_TYPE_FLASHRAM:
+    //         type = SAVE_TYPE_FLASHRAM;
+    //         break;
+    //     default:
+    //         return FLASHCART_ERROR_ARGS;
+    // }
 
     // if (ed64_set_config(CFG_SAVE_TYPE, type) != ED64_OK) {
     //     return FLASHCART_ERROR_INT;
     // }
 
-    ed64_bios_set_save_type(type);
+    // ed64_bios_set_save_type(type);
 
     return FLASHCART_OK;
 }

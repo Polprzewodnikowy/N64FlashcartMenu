71c93c516972aec4a2dafd88a7675d228f99d6a6
Robin Jones <networkfusion@users.noreply.github.com>
2024-03-07 23:17:39 +0000

    minor improvements

--- src/menu/rom_patcher.c
+++ src/menu/rom_patcher.c
@@ -8,11 +8,18 @@ rom_patcher_err_t rom_patcher_load_file (char *path)
 {
     // ROM file should be loaded befoe patch is applied.
     // apply patch dependent on extension.
-    //return apply_patch_type_ips(path);
     //return apply_patch_type_bps(path);
-    //return apply_patch_type_aps(path);
+    //return apply_patch_type_ips(path);
+    //return apply_patch_type_ups(path);
     //return apply_patch_type_xdelta(path);
+    // aps should be PATCH_ERR_UNSUPPORTED; as not really a thing?!
     return PATCH_ERR_IO;
+}
+
+rom_patcher_err_t apply_patch_type_bps(char *path)
+{
+    // https://github.com/Alcaro/Flips/blob/master/bps_spec.md
+    return PATCH_ERR_INVALID;
 }
 
 rom_patcher_err_t apply_patch_type_ips(char *path)
@@ -20,12 +27,7 @@ rom_patcher_err_t apply_patch_type_ips(char *path)
     return PATCH_ERR_INVALID;
 }
 
-rom_patcher_err_t apply_patch_type_bps(char *path)
-{
-    return PATCH_ERR_INVALID;
-}
-
-rom_patcher_err_t apply_patch_type_aps(char *path)
+rom_patcher_err_t apply_patch_type_ups(char *path)
 {
     return PATCH_ERR_INVALID;
 }
--- src/menu/views/browser.c
+++ src/menu/views/browser.c
@@ -11,7 +11,7 @@
 
 static const char *rom_extensions[] = { "z64", "n64", "v64", "rom", NULL };
 static const char *disk_extensions[] = { "ndd", NULL };
-static const char *patch_extensions[] = { "bps", "ips", "aps", "xdelta", NULL };
+static const char *patch_extensions[] = { "bps", "ips", "ups", "xdelta", NULL };
 static const char *emulator_extensions[] = { "nes", "sfc", "smc", "gb", "gbc", "sms", "gg", "sg", NULL };
 static const char *save_extensions[] = { "sav", NULL }; // TODO: "eep", "sra", "srm", "fla" could be used if transfered from different flashcarts.
 static const char *image_extensions[] = { "png", NULL };

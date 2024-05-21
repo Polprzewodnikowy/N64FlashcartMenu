9d26020d0477f4bcd51222be4180236de5bb0c81
Robin Jones <networkfusion@users.noreply.github.com>
2024-05-21 23:31:29 +0100

    Work on RTC

--- src/menu/views/rtc.c
+++ src/menu/views/rtc.c
@@ -1,6 +1,10 @@
 #include <time.h>
 #include "../sound.h"
 #include "views.h"
+#include <stdio.h>
+#include <malloc.h>
+#include <string.h>
+#include <libdragon.h>
 
 // FIXME: add implementation!
 // struct {
@@ -12,6 +16,58 @@
 //     uint16_t year;
 // } adjusted_datetime;
 
+typedef enum {
+    RTC_EDIT_YEAR,
+    RTC_EDIT_MONTH,
+    RTC_EDIT_DAY,
+    RTC_EDIT_HOUR,
+    RTC_EDIT_MIN,
+    RTC_EDIT_SEC,
+    RTC_EDIT_NONE,
+} rtc_editor_t;
+
+
+static rtc_time_t rtc_time;
+static uint16_t edit_mode = RTC_EDIT_NONE;
+
+int wrap( uint16_t val, uint16_t min, uint16_t max )
+{
+    if( val < min ) return max;
+    if( val > max ) return min;
+    return val;
+}
+
+void adjust_rtc_time( rtc_time_t* dt, int incr )
+{
+    uint8_t expected_day = 0;
+    switch( edit_mode )
+    {
+        case RTC_EDIT_YEAR:
+            /* TODO Figure out what the max supported year is */
+            dt->year = wrap( dt->year + incr, 1996, 2037 );
+            break;
+        case RTC_EDIT_MONTH:
+            dt->month = wrap( dt->month + incr, 0, 11 );
+            break;
+        case RTC_EDIT_DAY:
+            dt->day = wrap( dt->day + incr, 1, 31 );
+            expected_day = dt->day;
+            break;
+        case RTC_EDIT_HOUR:
+            dt->hour = wrap( dt->hour + incr, 0, 23 );
+            break;
+        case RTC_EDIT_MIN:
+            dt->min = wrap( dt->min + incr, 0, 59 );
+            break;
+        case RTC_EDIT_SEC:
+            dt->sec = wrap( dt->sec + incr, 0, 59 );
+            break;
+    }
+    rtc_normalize_time( dt );
+    /* Handle wrap-around for normalized day of month */
+    if( expected_day && expected_day != dt->day && incr > 0 ) dt->day = 1;
+}
+
 // static void save_adjusted_datetime () {
 
 // }
@@ -21,6 +77,23 @@ static void process (menu_t *menu) {
         menu->next_mode = MENU_MODE_BROWSER;
         sound_play_effect(SFX_EXIT);
     }
+    // FIXME: these are submenu items
+    if (menu->actions.enter) {
+        menu->next_mode = MENU_MODE_BROWSER;
+    }
+    if (menu->actions.go_left) {
+        menu->next_mode = MENU_MODE_BROWSER;
+    }
+    if (menu->actions.go_right) {
+        menu->next_mode = MENU_MODE_BROWSER;
+    }
+    if (menu->actions.go_up) {
+        menu->next_mode = MENU_MODE_BROWSER;
+    }
+    if (menu->actions.go_down) {
+        menu->next_mode = MENU_MODE_BROWSER;
+    }
+
 }
 
 static void draw (menu_t *menu, surface_t *d) {
@@ -30,16 +103,30 @@ static void draw (menu_t *menu, surface_t *d) {
 
     component_layout_draw();
 
-	component_main_text_draw(
-        ALIGN_CENTER, VALIGN_TOP,
-        "ADJUST REAL TIME CLOCK\n"
-        "\n"
-        "\n"
-        "To set the date and time, please use the PC terminal\n"
-        "application and set via USB.\n\n"
-        "Current date & time: %s\n",
-        menu->current_time >= 0 ? ctime(&menu->current_time) : "Unknown\n"
-    );
+    if (rtc_is_writable()) {
+        component_main_text_draw(
+            ALIGN_CENTER, VALIGN_TOP,
+            "ADJUST REAL TIME CLOCK\n"
+            "\n"
+            "\n"
+            "To set the date and time, please use the PC terminal\n"
+            "application and set via USB.\n\n"
+            "Current date & time: %s\n",
+            menu->current_time >= 0 ? ctime(&menu->current_time) : "Unknown\n"
+        );
+    }
+    else {
+        // FIXME: time since N64 started?
+        component_main_text_draw(
+            ALIGN_CENTER, VALIGN_TOP,
+            "REAL TIME CLOCK\n"
+            "\n"
+            "\n"
+            "Adjusting the date and time is not supported.\n"
+            "Current date & time: %s\n",
+            menu->current_time >= 0 ? ctime(&menu->current_time) : "Unknown\n"
+        );
+    }
 
     component_main_text_draw(
         ALIGN_LEFT, VALIGN_TOP,
@@ -47,12 +134,20 @@ static void draw (menu_t *menu, surface_t *d) {
         "\n"
     );
 
-
-    component_actions_bar_text_draw(
-        ALIGN_LEFT, VALIGN_TOP,
-        "\n" // "A: Save\n"
-        "B: Back"
-    );
+    if (rtc_is_writable()) {
+        component_actions_bar_text_draw(
+            ALIGN_LEFT, VALIGN_TOP,
+            "A: Save\n"
+            "B: Back"
+        );
+    }
+    else {
+        component_actions_bar_text_draw(
+            ALIGN_LEFT, VALIGN_TOP,
+            "\n"
+            "B: Back"
+        );
+    }
 
     rdpq_detach_show();
 }

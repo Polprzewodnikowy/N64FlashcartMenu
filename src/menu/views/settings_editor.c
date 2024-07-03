249cab94dd5524ce21f6ab85e8d1f2f7b86bef3c
Robin Jones <networkfusion@users.noreply.github.com>
2024-07-03 19:12:11 +0100

    Text improvement

--- src/menu/views/settings_editor.c
+++ src/menu/views/settings_editor.c
@@ -114,7 +114,7 @@ static void draw (menu_t *menu, surface_t *d) {
         "rumble_enabled:         %s\n\n"
         "Note: Certain settings are only adjustable\n"
         "directly in the 'menu/config.ini' file.\n\n"
-        "*  Set using other menu.\n"
+        "*  Set using browser submenu.\n"
         "** Currently requires a flashcart reboot.\n",
         format_switch(menu->settings.pal60_enabled),
         format_switch(menu->settings.show_protected_entries),

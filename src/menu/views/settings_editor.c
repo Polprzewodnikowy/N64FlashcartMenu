00f24a971b514a0e4b98899d60005c31bdecfcf7
Robin Jones <networkfusion@users.noreply.github.com>
2024-07-03 16:21:57 +0100

    Add context menu for settings
    TODO: save settings to config.ini

--- src/menu/views/settings_editor.c
+++ src/menu/views/settings_editor.c
@@ -9,11 +9,75 @@ static const char *format_switch (bool state) {
     }
 }
 
+static void set_pal60_type (menu_t *menu, void *arg) {
+    menu->settings.pal60_enabled = (bool) (arg);
+    
+    menu->browser.reload = true;
+}
+
+static void set_protected_entries_type (menu_t *menu, void *arg) {
+    menu->settings.show_protected_entries = (bool) (arg);
+    
+    menu->browser.reload = true;
+}
+
+static void set_sound_enabled_type (menu_t *menu, void *arg) {
+    menu->settings.sound_enabled = (bool) (arg);
+    
+    menu->browser.reload = true;
+}
+
+static void set_use_saves_folder_type (menu_t *menu, void *arg) {
+    menu->settings.use_saves_folder = (bool) (arg);
+    
+    menu->browser.reload = true;
+}
+
+static component_context_menu_t set_pal60_type_context_menu = { .list = {
+    {.text = "Enable", .action = set_pal60_type, .arg = (void *) (true) },
+    {.text = "Disable", .action = set_pal60_type, .arg = (void *) (false) },
+    COMPONENT_CONTEXT_MENU_LIST_END,
+}};
+
+static component_context_menu_t set_protected_entries_type_context_menu = { .list = {
+    {.text = "Enable", .action = set_protected_entries_type, .arg = (void *) (true) },
+    {.text = "Disable", .action = set_protected_entries_type, .arg = (void *) (false) },
+    COMPONENT_CONTEXT_MENU_LIST_END,
+}};
+
+static component_context_menu_t set_sound_enabled_type_context_menu = { .list = {
+    {.text = "Enable", .action = set_sound_enabled_type, .arg = (void *) (true) },
+    {.text = "Disable", .action = set_sound_enabled_type, .arg = (void *) (false) },
+    COMPONENT_CONTEXT_MENU_LIST_END,
+}};
+
+static component_context_menu_t set_use_saves_folder_type_context_menu = { .list = {
+    {.text = "Enable", .action = set_use_saves_folder_type, .arg = (void *) (true) },
+    {.text = "Disable", .action = set_use_saves_folder_type, .arg = (void *) (false) },
+    COMPONENT_CONTEXT_MENU_LIST_END,
+}};
+
+static component_context_menu_t options_context_menu = { .list = {
+    { .text = "Set PAL60 Mode", .submenu = &set_pal60_type_context_menu },
+    { .text = "Show Hidden Files", .submenu = &set_protected_entries_type_context_menu },
+    { .text = "Set Menu Sound", .submenu = &set_sound_enabled_type_context_menu },
+    { .text = "Use saves folder", .submenu = &set_use_saves_folder_type_context_menu },
+
+    COMPONENT_CONTEXT_MENU_LIST_END,
+}};
+
 
 static void process (menu_t *menu) {
+    if (component_context_menu_process(menu, &options_context_menu)) {
+        return;
+    }
+
     if (menu->actions.back) {
         menu->next_mode = MENU_MODE_BROWSER;
         sound_play_effect(SFX_EXIT);
+    } else if (menu->actions.options) {
+        component_context_menu_show(&options_context_menu);
+        sound_play_effect(SFX_SETTING);
     }
 }
 
@@ -34,7 +98,8 @@ static void draw (menu_t *menu, surface_t *d) {
         ALIGN_LEFT, VALIGN_TOP,
         "\n"
         "\n"
-        "To change the settings, please adjust them\n"
+        "To change the settings, press `R`.\n"
+        "Certain options are only adjustable\n"
         "directly in the 'menu/config.ini' file.\n\n"
         "pal60_enabled:          %s\n"
         "show_protected_entries: %s\n"
@@ -58,12 +123,22 @@ static void draw (menu_t *menu, surface_t *d) {
         "B: Back"
     );
 
+    component_actions_bar_text_draw(
+            ALIGN_RIGHT, VALIGN_TOP,
+            "\n"
+            "R: Options"
+        );
+
+    component_context_menu_draw(&options_context_menu);
+
     rdpq_detach_show();
 }
 
 
 void view_settings_init (menu_t *menu) {
-    // Nothing to initialize (yet)
+    
+    component_context_menu_init(&options_context_menu);
+
 }
 
 void view_settings_display (menu_t *menu, surface_t *display) {

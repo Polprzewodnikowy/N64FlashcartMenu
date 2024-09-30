a84db5cf4d92f1d5dad2b9ac35c85da5c9c65ca5
Robin Jones <networkfusion@users.noreply.github.com>
2024-09-30 22:08:43 +0100

    Apply suggestions from code review

--- src/menu/views/fault.c
+++ src/menu/views/fault.c
@@ -7,10 +7,10 @@ static void draw (menu_t *menu, surface_t *d) {
     rdpq_clear(RGBA32(0x7F, 0x00, 0x00, 0xFF));
 
     const char *firmware_message = (
-        "Supported firmware versions:\n"
+        "Minimum supported firmware versions:\n"
         "64drive: 2.05+\n"
         "EverDrive-64: ???+\n"
-        "SummerCart64: 2.20.0+"
+        "SummerCart64: 2.17.0+"
     );
 
     component_messagebox_draw(

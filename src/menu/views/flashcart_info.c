6eb6991ca02edd82b1b9ce45af4673db13f61631
Robin Jones <networkfusion@users.noreply.github.com>
2024-10-25 14:48:21 +0100

    Detect and display flashcart features correctly

--- src/menu/views/flashcart_info.c
+++ src/menu/views/flashcart_info.c
@@ -1,9 +1,29 @@
 #include "views.h"
 #include "../sound.h"
+#include <libcart/cart.h>
 
 
 static inline const char *format_boolean_type (bool bool_value) {
     return bool_value ? "Supported" : "Unsupported";
+}
+
+static const char *format_cart_type () {
+    switch (cart_type) {
+        case CART_CI:
+            return "64drive";
+
+        case CART_EDX:
+            return "Series X EverDrive-64";
+
+        case CART_ED:
+            return "Series V EverDrive-64";
+
+        case CART_SC:
+            return "SummerCart64";
+
+        default:        // Probably emulator
+            return "Emulator?";
+    }
 }
 
 static void process (menu_t *menu) {
@@ -42,20 +62,14 @@ static void draw (menu_t *menu, surface_t *d) {
         "  CIC Detection:    %s.\n"
         "  Region Detection: %s.\n"
         "\n\n",
-        "SummerCart64",
-        "V?.?.?",
-        format_boolean_type(true),
-        format_boolean_type(true),
-        format_boolean_type(true),
-        format_boolean_type(true),
-        format_boolean_type(true)
+        format_cart_type(),
+        "Not Available",
+        format_boolean_type(flashcart_has_feature(FLASHCART_FEATURE_64DD)),
+        format_boolean_type(flashcart_has_feature(FLASHCART_FEATURE_RTC)),
+        format_boolean_type(flashcart_has_feature(FLASHCART_FEATURE_USB)),
+        format_boolean_type(flashcart_has_feature(FLASHCART_FEATURE_AUTO_CIC)),
+        format_boolean_type(flashcart_has_feature(FLASHCART_FEATURE_AUTO_REGION))
     );
-
-    // FIXME: Display:
-    // * cart_type
-    // * Firmware version
-    // * supported features (flashcart_features_t)
-
 
     component_actions_bar_text_draw(
         ALIGN_LEFT, VALIGN_TOP,

5d62b165159a73cc2e918019ed8efebf658b23ec
Robin Jones <networkfusion@users.noreply.github.com>
2024-11-04 00:26:31 +0000

    start cleanup

--- src/menu/views/startup.c
+++ src/menu/views/startup.c
@@ -22,30 +22,11 @@ void view_startup_init (menu_t *menu) {
         }
     }
     if (menu->settings.rom_autoload_enabled) {
-        //dir_t info;
         path_t *path = path_init(menu->storage_prefix, menu->settings.rom_autoload_path);
 
         menu->browser.directory = path_clone(path);
-        //menu->browser.list = realloc(menu->browser.list, (menu->browser.entries + 1) * sizeof(entry_t));
-        // entry_t entry; // = &menu->browser.list[20];
 
-        //dir_findfirst(path_get(path), &info);
-
-        //menu->browser.selected = 0;
-
-        //menu->browser.entry = &menu->browser.list[menu->browser.selected];
-
-        // entry.name = strdup(menu->settings.rom_autoload_filename);
-        // entry.type = ENTRY_TYPE_ROM;
-        // menu->browser.entry = &entry;
-       // menu->browser.entry->name = strdup(menu->settings.rom_autoload_filename);
         menu->load.rom_path = path_clone_push(menu->browser.directory, menu->settings.rom_autoload_filename);
-
-        // entry_t *autoload_entry;
-        // autoload_entry->name = strdup(menu->settings.rom_autoload_entry);
-        // autoload_entry->type = ENTRY_TYPE_ROM;
-        //menu->browser.entry = (entry_t); //autoload_entry;
-        // 
 
         menu->boot_pending.rom_file = true;
         menu->next_mode = MENU_MODE_LOAD_ROM;

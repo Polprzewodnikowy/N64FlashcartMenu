164848a531137297a42cde0c84801c325e28ae3a
Robin Jones <networkfusion@users.noreply.github.com>
2024-11-22 18:38:52 +0000

    Improve save type
    smsPlus64 does not support one yet.

--- src/menu/cart_load.c
+++ src/menu/cart_load.c
@@ -178,7 +178,7 @@ cart_load_err_t cart_load_emulator (menu_t *menu, cart_load_emu_type_t emu_type,
             break;
         case CART_LOAD_EMU_TYPE_SEGA_GENERIC_8BIT:
             path_push(path, "smsPlus64.z64");
-            save_type = FLASHCART_SAVE_TYPE_SRAM_256KBIT;
+            save_type = FLASHCART_SAVE_TYPE_NONE;
             break;
     }
 

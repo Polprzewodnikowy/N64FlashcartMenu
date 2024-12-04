42b9bd7e07a278ebf122467bcf5c56a4c8c62ac1
Ross Gouldthorpe <github@thegouldfish.co.uk>
2024-12-04 13:16:02 +0000

    Update menu.c
    Removing the empty entry that snuck in

--- src/menu/menu.c
+++ src/menu/menu.c
@@ -158,8 +158,7 @@ static view_t menu_views[] = {
     { MENU_MODE_LOAD_EMULATOR, view_load_emulator_init, view_load_emulator_display },
     { MENU_MODE_ERROR, view_error_init, view_error_display },
     { MENU_MODE_FAULT, view_fault_init, view_fault_display },
-    { MENU_MODE_FAVORITE, view_favorite_init, view_favorite_display },
-    {}
+    { MENU_MODE_FAVORITE, view_favorite_init, view_favorite_display }
 };
 
 static view_t *menu_get_view (menu_mode_t id) {

a7669b1057de1d9634aef2e093de3e068fbf46e4
Robin Jones <networkfusion@users.noreply.github.com>
2024-12-04 12:30:38 +0000

    Add Fairchild Channel F emulator support (#166)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Description
    <!--- Describe your changes in detail -->
    Adds the required changes for supporting the Fairchild Channel F
    emulator.
    
    ## Motivation and Context
    <!--- What does this sample do? What problem does it solve? -->
    <!--- If it fixes/closes/resolves an open issue, please link to the
    issue here -->
    Support more emulators.
    
    ## How Has This Been Tested?
    <!-- (if applicable) -->
    <!--- Please describe in detail how you tested your sample/changes. -->
    <!--- Include details of your testing environment, and the tests you ran
    to -->
    <!--- see how your change affects other areas of the code, etc. -->
    Still required.
    
    ## Screenshots
    <!-- (if appropriate): -->
    
    ## Types of changes
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    - [x] Improvement (non-breaking change that adds a new feature)
    - [ ] Bug fix (fixes an issue)
    - [ ] Breaking change (breaking change)
    - [ ] Documentation Improvement
    - [ ] Config and build (change in the configuration and build system,
    has no impact on code or features)
    
    ## Checklist:
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    - [x] My code follows the code style of this project.
    - [ ] My change requires a change to the documentation.
    - [x] I have updated the documentation accordingly.
    - [ ] I have added tests to cover my changes.
    - [ ] All new and existing tests passed.
    
    <!--- It would be nice if you could sign off your contribution by
    replacing the name with your GitHub user name and GitHub email contact.
    -->
    Signed-off-by: GITHUB_USER <GITHUB_USER_EMAIL>

--- src/menu/cart_load.c
+++ src/menu/cart_load.c
@@ -180,6 +180,10 @@ cart_load_err_t cart_load_emulator (menu_t *menu, cart_load_emu_type_t emu_type,
             path_push(path, "smsPlus64.z64");
             save_type = FLASHCART_SAVE_TYPE_NONE;
             break;
+        case CART_LOAD_EMU_TYPE_FAIRCHILD_CHANNELF:
+            path_push(path, "Press-F.z64");
+            save_type = FLASHCART_SAVE_TYPE_NONE;
+            break;
     }
 
     if (!file_exists(path_get(path))) {

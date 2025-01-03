2b07f782f4a089ebb87bd13db562e1d6cc1d36e0
Robin Jones <networkfusion@users.noreply.github.com>
2025-01-03 03:17:13 +0000

    Update bookkeeping.h
    Documentation improvements

--- src/menu/bookkeeping.h
+++ src/menu/bookkeeping.h
@@ -1,11 +1,11 @@
 /**
- * @file rom_history.h
- * @brief Menu History
+ * @file bookkeeping.h
+ * @brief Bookkeeping of loadded ROM's.
  * @ingroup menu 
  */
 
-#ifndef HISTORY_H__
-#define HISTORY_H__
+#ifndef BOOKKEEPING_H__
+#define BOOKKEEPING_H__
 
 #include "path.h"
 
@@ -27,7 +27,7 @@ typedef struct {
 
 } bookkeeping_item_t;
 
-/** @brief history Structure */
+/** @brief ROM bookkeeping Structure */
 typedef struct {
     bookkeeping_item_t history_items[HISTORY_COUNT];
 
@@ -35,18 +35,41 @@ typedef struct {
 } bookkeeping_t;
 
 
-/** @brief Init history path */
+/** @brief Init ROM bookkeeping path */
 void bookkeeping_init (char *path);
 
-/** @brief The history to load */
+/** @brief The ROM bookkeeping to load */
 void bookkeeping_load (bookkeeping_t *history);
 
-/** @brief The history to save */
+/** @brief The ROM bookkeeping to save */
 void bookkeeping_save (bookkeeping_t *history);
 
+/**
+ * @brief Add a ROM to the history.
+ * 
+ * @param bookkeeping The bookkeeping structure.
+ * @param primary_path The primary path of the ROM.
+ * @param secondary_path The secondary path of the ROM.
+ * @param type The type of the bookkeeping item.
+ */
 void bookkeeping_history_add(bookkeeping_t *bookkeeping, path_t *primary_path, path_t *secondary_path, bookkeeping_item_types_t type );
 
+/**
+ * @brief Add a ROM to the favorites.
+ * 
+ * @param bookkeeping The bookkeeping structure.
+ * @param primary_path The primary path of the ROM.
+ * @param secondary_path The secondary path of the ROM.
+ * @param type The type of the bookkeeping item.
+ */
 void bookkeeping_favorite_add(bookkeeping_t *bookkeeping, path_t *primary_path, path_t *secondary_path, bookkeeping_item_types_t type );
+
+/**
+ * @brief Remove a ROM from the favorites.
+ * 
+ * @param bookkeeping The bookkeeping structure.
+ * @param selection The index of the favorite item to remove.
+ */
 void bookkeeping_favorite_remove(bookkeeping_t *bookkeeping, int selection);
 
 #endif

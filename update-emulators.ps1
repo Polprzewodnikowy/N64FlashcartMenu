8573afb099842be1ee765db764bee9fdbbfb8e5d
Robin Jones <networkfusion@users.noreply.github.com>
2025-06-06 23:54:51 +0100

    Update update-emulators.ps1
    Move URL's to top of file.

--- update-emulators.ps1
+++ update-emulators.ps1
@@ -3,26 +3,29 @@ $ErrorActionPreference = "Stop"
 $emulatorDir = "filesystem/menu/emulators"
 New-Item -ItemType Directory -Path $emulatorDir -Force | Out-Null
 
-# Get neon64
+# URLs for the emulators using the latest versions as of the time of writing.
+# If the URLs change or versions need updating, you will need to update them accordingly.
 $neon64Url = "https://github.com/hcs64/neon64v2/releases/download/v2.0-beta.4/neon64v2b4.zip"
+$sodium64Url = "https://github.com/Hydr8gon/sodium64/releases/download/release/sodium64.zip"
+$smsplus64Url = "https://github.com/fhoedemakers/smsplus64/releases/download/v0.7/smsPlus64.z64"
+$pressfUrl = "https://github.com/celerizer/Press-F-Ultra/releases/download/r4/Press-F.z64"
+
+# Get neon64
 $neon64Zip = "$env:TEMP/neon64v2b4.zip"
 Invoke-WebRequest -Uri $neon64Url -OutFile $neon64Zip
 Expand-Archive -Path $neon64Zip -DestinationPath $env:TEMP/neon64v2b4 -Force
 Copy-Item -Path (Get-ChildItem -Path "$env:TEMP/neon64v2b4" -Recurse -Filter "neon64bu.rom").FullName -Destination $emulatorDir -Force
 
 # Get sodium64
-$sodium64Url = "https://github.com/Hydr8gon/sodium64/releases/download/release/sodium64.zip"
 $sodium64Zip = "$env:TEMP/sodium64.zip"
 Invoke-WebRequest -Uri $sodium64Url -OutFile $sodium64Zip
 Expand-Archive -Path $sodium64Zip -DestinationPath $env:TEMP/sodium64 -Force
 Copy-Item -Path (Get-ChildItem -Path "$env:TEMP/sodium64" -Recurse -Filter "sodium64.z64").FullName -Destination $emulatorDir -Force
 
 # Get smsPlus64
-$smsplus64Url = "https://github.com/fhoedemakers/smsplus64/releases/download/v0.7/smsPlus64.z64"
 Invoke-WebRequest -Uri $smsplus64Url -OutFile "$emulatorDir/smsplus64.z64"
 
 # Get Press-F Ultra
-$pressfUrl = "https://github.com/celerizer/Press-F-Ultra/releases/download/r4/Press-F.z64"
 Invoke-WebRequest -Uri $pressfUrl -OutFile "$emulatorDir/pressf.z64"
 
 # Get gb64

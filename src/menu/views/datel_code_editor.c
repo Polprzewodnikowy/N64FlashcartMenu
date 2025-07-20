12d858c1ef5fd5c7bab5bb58527669835547f689
Robin Jones <networkfusion@users.noreply.github.com>
2025-07-20 16:15:59 +0100

    Add GUI warning

--- src/menu/views/datel_code_editor.c
+++ src/menu/views/datel_code_editor.c
@@ -17,10 +17,18 @@ static void toggle_enable_selected_cheat (menu_t *menu, void *arg) {
 
 static void edit_selected_cheat_address (menu_t *menu, void *arg) {
     debugf("Cheat Editor: Edit Selected Cheat address not implemented yet.\n");
+    menu_show_error(menu,
+        "Edit Selected Cheat address...\n"
+        "feature not implemented yet.\n"
+    );
 }
 
 static void edit_selected_cheat_value (menu_t *menu, void *arg) {
     debugf("Cheat Editor: Edit Selected Cheat value not implemented yet.\n");
+    menu_show_error(menu,
+        "Edit Selected Cheat value...\n"
+        "feature not implemented yet.\n"
+    );
 }
 
 static void reset_selected_cheat (menu_t *menu, void *arg) {

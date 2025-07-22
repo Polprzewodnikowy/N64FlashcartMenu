18e63b628db60ea87d4e5221236bc68afc31a980
Robin Jones <networkfusion@users.noreply.github.com>
2025-07-22 23:16:22 +0100

    Work towards cheat address and value editing

--- src/menu/views/datel_code_editor.c
+++ src/menu/views/datel_code_editor.c
@@ -1,11 +1,57 @@
 #include "../datel_codes.h"
 #include "views.h"
 #include "../sound.h"
-#include "../ui_components/constants.h"
+#include "../ui_components/constants.h" // FIXME: remove when ui_component_value_editor can handle widths by itself.
 
 
 static cheat_file_code_t *cheat_codes;
 static short item_selected = 0;
+static bool is_editing_mode_address = false;
+static bool is_editing_mode_value = false;
+static uint8_t editing_field_selected = 7; // 0-7 for 8 nibbles of the address or value (set to last nibble by default).
+
+typedef struct {
+    char slot_zero[2];
+    char slot_one[2];
+    char slot_two[2];
+    char slot_three[2];
+    char slot_four[2];
+    char slot_five[2];
+    char slot_six[2];
+    char slot_seven[2];
+} cheat_field_nibbles_t;
+
+/**
+ * Draws the cheat editor UI component.
+ * 
+ * @param address The address to display.
+ * @param selected_field The field currently selected for editing.
+ */
+static void cheat_ui_component_edit_field_draw ( uint32_t address, int selected_field ) {
+    cheat_field_nibbles_t nibbles;
+
+    /* Format each nibble in address as hex */
+    snprintf(nibbles.slot_zero, sizeof(nibbles.slot_zero), "%lX", (address >> 28) & 0xF);
+    snprintf(nibbles.slot_one, sizeof(nibbles.slot_one), "%lX", (address >> 24) & 0xF);
+    snprintf(nibbles.slot_two, sizeof(nibbles.slot_two), "%lX", (address >> 20) & 0xF);
+    snprintf(nibbles.slot_three, sizeof(nibbles.slot_three), "%lX", (address >> 16) & 0xF);
+    snprintf(nibbles.slot_four, sizeof(nibbles.slot_four), "%lX", (address >> 12) & 0xF);
+    snprintf(nibbles.slot_five, sizeof(nibbles.slot_five), "%lX", (address >> 8) & 0xF);
+    snprintf(nibbles.slot_six, sizeof(nibbles.slot_six), "%lX", (address >> 4) & 0xF);
+    snprintf(nibbles.slot_seven, sizeof(nibbles.slot_seven), "%lX", address & 0xF);
+
+    ui_component_value_editor(
+        (const char *[]){
+            "*", "*", "*", "*", "*", "*", "*", "*"
+        },
+        (const char *[]){
+            nibbles.slot_zero, nibbles.slot_one, nibbles.slot_two, nibbles.slot_three, nibbles.slot_four, nibbles.slot_five, nibbles.slot_six, nibbles.slot_seven
+        },
+        8,
+        selected_field,
+        (VISIBLE_AREA_WIDTH - (TEXT_MARGIN_HORIZONTAL * 2)) / 20.0f
+    );
+}
 
 
 static void toggle_enable_selected_cheat (menu_t *menu, void *arg) {
@@ -16,19 +62,15 @@ static void toggle_enable_selected_cheat (menu_t *menu, void *arg) {
 }
 
 static void edit_selected_cheat_address (menu_t *menu, void *arg) {
-    debugf("Cheat Editor: Edit Selected Cheat address %08lX, not implemented yet.\n", cheat_codes[item_selected].address);
-    menu_show_error(menu,
-        "Edit Selected Cheat address...\n"
-        "feature not implemented yet.\n"
-    );
+    debugf("Cheat Editor: Edit Selected Cheat address %08lX, edit not implemented.\n", cheat_codes[item_selected].address);
+    editing_field_selected = 0; // Reset to the first nibble of the address.
+    is_editing_mode_address = true;
 }
 
 static void edit_selected_cheat_value (menu_t *menu, void *arg) {
-    debugf("Cheat Editor: Edit Selected Cheat value: %04X, not implemented yet.\n", cheat_codes[item_selected].value);
-    menu_show_error(menu,
-        "Edit Selected Cheat value...\n"
-        "feature not implemented yet.\n"
-    );
+    debugf("Cheat Editor: Edit Selected Cheat value: %04X, edit not implemented.\n", cheat_codes[item_selected].value);
+    editing_field_selected = 4; // Reset to the first nibble of the value.
+    is_editing_mode_value = true;
 }
 
 static void reset_selected_cheat (menu_t *menu, void *arg) {
@@ -62,29 +104,56 @@ static void process(menu_t *menu) {
         return;
     }
 
-    if(menu->actions.go_down) {
-        item_selected++;
-        if (item_selected >= MAX_CHEAT_CODES) {
-            item_selected = 0;
+    if (is_editing_mode_address || is_editing_mode_value) {
+        if (menu->actions.go_left) {
+            if ( editing_field_selected <= 0 ) { editing_field_selected = 7; }
+            else { editing_field_selected = editing_field_selected - 1; }
         }
-        sound_play_effect(SFX_CURSOR);
-    } else if(menu->actions.go_up) {
-        item_selected--;
-        if (item_selected < 0) {
-            item_selected = MAX_CHEAT_CODES - 1;
+        else if (menu->actions.go_right) {
+            if ( editing_field_selected >= 7 ) { editing_field_selected = 0; }
+            else { editing_field_selected = editing_field_selected + 1; }
+       
         }
-        sound_play_effect(SFX_CURSOR);
-    } else if(menu->actions.enter) {
-        set_cheat_codes(cheat_codes);
-        menu->next_mode = MENU_MODE_LOAD_ROM;
-        debugf("Cheat Editor: Applying cheats.\n");
-        sound_play_effect(SFX_ENTER);
-    } else if (menu->actions.back) {
-        sound_play_effect(SFX_EXIT);
-        menu->next_mode = MENU_MODE_BROWSER;
-    } else if (menu->actions.options) {
-        ui_components_context_menu_show(&options_context_menu);
-        sound_play_effect(SFX_SETTING);
+        else if (menu->actions.go_up) {
+            // adjust_selected_nibble( &cheat_field_nibble, +1 );
+        }
+        else if (menu->actions.go_down) {
+            // adjust_selected_nibble( &cheat_field_nibble, -1 );
+        }
+        else if (menu->actions.enter) { // apply
+            is_editing_mode_address = false;
+            is_editing_mode_value = false;
+        }
+        else if (menu->actions.back) { // cancel
+            is_editing_mode_address = false;
+            is_editing_mode_value = false;
+        }
+    } 
+    else {
+        if(menu->actions.go_down) {
+            item_selected++;
+            if (item_selected >= MAX_CHEAT_CODES) {
+                item_selected = 0;
+            }
+            sound_play_effect(SFX_CURSOR);
+        } else if(menu->actions.go_up) {
+            item_selected--;
+            if (item_selected < 0) {
+                item_selected = MAX_CHEAT_CODES - 1;
+            }
+            sound_play_effect(SFX_CURSOR);
+        } else if(menu->actions.enter) {
+            set_cheat_codes(cheat_codes);
+            menu->next_mode = MENU_MODE_LOAD_ROM;
+            debugf("Cheat Editor: Applying cheats.\n");
+            sound_play_effect(SFX_ENTER);
+        } else if (menu->actions.back) {
+            sound_play_effect(SFX_EXIT);
+            menu->next_mode = MENU_MODE_BROWSER;
+        } else if (menu->actions.options) {
+            ui_components_context_menu_show(&options_context_menu);
+            sound_play_effect(SFX_SETTING);
+        }
     }
 }
 
@@ -278,10 +347,27 @@ static void draw (menu_t *menu, surface_t *display) {
 
     ui_components_context_menu_draw(&options_context_menu);
 
+    if (is_editing_mode_address) {
+        cheat_ui_component_edit_field_draw(
+            cheat_codes[item_selected].address,
+            editing_field_selected
+        );
+    }
+
+    if (is_editing_mode_value) {
+        cheat_ui_component_edit_field_draw(
+            cheat_codes[item_selected].value,
+            editing_field_selected
+        );
+    }
+
     rdpq_detach_show();
 }
 
 void view_datel_code_editor_init (menu_t *menu) {
+    is_editing_mode_address = false;
+    is_editing_mode_value = false;
+
     ui_components_context_menu_init(&options_context_menu);
     ui_components_context_menu_init(&cm_edit_selected_cheat);
 

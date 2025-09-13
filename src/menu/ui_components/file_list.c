f2688e36ac6eaf114467ed61858d39148cea2913
LuEnCam <67584779+LuEnCam@users.noreply.github.com>
2025-09-13 22:01:22 +0200

    reset file_list after merging mess

--- src/menu/ui_components/file_list.c
+++ src/menu/ui_components/file_list.c
@@ -59,7 +59,8 @@ void ui_components_file_list_draw (entry_t *list, int entries, int selected) {
             STL_DEFAULT,
             ALIGN_LEFT, VALIGN_TOP,
             "\n"
-            "** empty directory **"
+            "^%02X** empty directory **",
+            STL_GRAY
         );
     } else {
         rdpq_paragraph_t *file_list_layout;

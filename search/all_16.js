2fdf3b42638fece8916d558eecb826ded88533a5
networkfusion <networkfusion@users.noreply.github.com>
2025-11-01 23:10:19 +0000

    deploy: aec181ca803a11a02e97aefa40b93031b7ff297e

--- search/all_16.js
+++ search/all_16.js
@@ -1,9 +1,9 @@
 var searchData=
 [
   ['n64_20flashcart_20menu_0',['N64 Flashcart Menu',['../index.html#autotoc_md0',1,'']]],
-  ['n64_20hardware_20modifications_20compatibility_1',['N64 Hardware Modifications Compatibility',['../md_docs_262__hardware__mods.html#autotoc_md98',1,'']]],
-  ['n64_20information_2',['N64 Information',['../md_docs_237__n64__information.html#autotoc_md92',1,'']]],
-  ['n64_20roms_3',['Autoloading N64 ROMs',['../md_docs_222__autoload__roms.html#autotoc_md77',1,'']]],
+  ['n64_20hardware_20modifications_20compatibility_1',['N64 Hardware Modifications Compatibility',['../md_docs_262__hardware__mods.html#autotoc_md99',1,'']]],
+  ['n64_20information_2',['N64 Information',['../md_docs_237__n64__information.html#autotoc_md93',1,'']]],
+  ['n64_20roms_3',['Autoloading N64 ROMs',['../md_docs_222__autoload__roms.html#autotoc_md78',1,'']]],
   ['n64_5faleck64_4',['N64_ALECK64',['../rom__info_8h.html#a0049f84e1fa62200145aa4bcbf7510b0ae05d1f3888937647e60af840097f32c0',1,'rom_info.h']]],
   ['n64_5fcart_5',['N64_CART',['../rom__info_8h.html#a0049f84e1fa62200145aa4bcbf7510b0a1bd823fc8b9c795053d238a96c6f5524',1,'rom_info.h']]],
   ['n64_5fcart_5fexpandable_6',['N64_CART_EXPANDABLE',['../rom__info_8h.html#a0049f84e1fa62200145aa4bcbf7510b0a45d9f8446e39a1fa4220d57448e609df',1,'rom_info.h']]],
@@ -12,10 +12,11 @@ var searchData=
   ['n64flashcartmenu_9',['N64FlashcartMenu',['../index.html',1,'']]],
   ['n64flashcartmenu_20settings_10',['N64FlashcartMenu settings',['../md_docs_211__menu__controls.html#autotoc_md48',1,'']]],
   ['n64flashcartmenu_20user_20guide_11',['N64FlashcartMenu User Guide',['../md_docs_200__index.html#autotoc_md30',1,'']]],
-  ['name_20here_20does_20not_20work_12',['ROM hack (insert hack name here) does not work',['../md_docs_281__faq.html#autotoc_md105',1,'']]],
-  ['native_20os_13',['To the native OS',['../md_docs_299__developer__guide.html#autotoc_md116',1,'']]],
-  ['not_20work_14',['ROM hack (insert hack name here) does not work',['../md_docs_281__faq.html#autotoc_md105',1,'']]],
-  ['not_20working_15',['I have changed the menu/config.ini file manually and things are not working.',['../md_docs_281__faq.html#autotoc_md108',1,'']]],
-  ['not_20yet_20planned_16',['Not yet planned',['../index.html#autotoc_md4',1,'']]],
-  ['notable_20menu_20features_17',['Current (notable) menu features',['../index.html#autotoc_md5',1,'']]]
+  ['name_20here_20does_20not_20work_12',['ROM hack (insert hack name here) does not work',['../md_docs_281__faq.html#autotoc_md106',1,'']]],
+  ['names_13',['Supported file names',['../md_docs_219__gamepak__boxart.html#autotoc_md75',1,'']]],
+  ['native_20os_14',['To the native OS',['../md_docs_299__developer__guide.html#autotoc_md117',1,'']]],
+  ['not_20work_15',['ROM hack (insert hack name here) does not work',['../md_docs_281__faq.html#autotoc_md106',1,'']]],
+  ['not_20working_16',['I have changed the menu/config.ini file manually and things are not working.',['../md_docs_281__faq.html#autotoc_md109',1,'']]],
+  ['not_20yet_20planned_17',['Not yet planned',['../index.html#autotoc_md4',1,'']]],
+  ['notable_20menu_20features_18',['Current (notable) menu features',['../index.html#autotoc_md5',1,'']]]
 ];

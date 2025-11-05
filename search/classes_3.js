46536eb9ad954c606e059b8470f95be0d73f6ced
networkfusion <networkfusion@users.noreply.github.com>
2025-11-05 22:30:04 +0000

    deploy: ec8e18160b1d58fe5906919d930e11e81885060d

--- search/classes_3.js
+++ search/classes_3.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['d64_5fregs_5ft_0',['d64_regs_t',['../group__drive.html#structd64__regs__t',1,'']]],
   ['disk_5finfo_5ft_1',['disk_info_t',['../disk__info_8h.html#structdisk__info__t',1,'']]],
-  ['dpc_5fregs_5ft_2',['dpc_regs_t',['../boot__io_8h.html#structdpc__regs__t',1,'']]]
+  ['disk_5fslot_5fentry_5ft_2',['disk_slot_entry_t',['../menu__state_8h.html#structdisk__slot__entry__t',1,'']]],
+  ['dpc_5fregs_5ft_3',['dpc_regs_t',['../boot__io_8h.html#structdpc__regs__t',1,'']]]
 ];

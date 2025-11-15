bc072415d5b34602bf33d4f7d324f2fd3cb6a567
Robin Jones <networkfusion@users.noreply.github.com>
2025-11-15 15:37:24 +0000

    Cheat engine improvements (#261)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Description
    <!--- Describe your changes in detail -->
    The way the code checks for the libdragon ipl3 is by reading a value
    that is known to be a jump instruction in the libultra ipl3
    but that area of CIC6106 is scrambled
    the person who wrote this code accounted for that when inserting the
    hook, but forgot about it when checking for the libdragon ipl3
    so the check reads 0x80A87642 from the ipl3, correctly determines that
    it's not a jump instruction, and assumes it must be libdragon
    the fix is to apply the same scrambling when CIC6106 is detected also
    when doing the check.
    
    This also fixes DE cheat types.
    And CIC_7102 handling.
    
    ## Motivation and Context
    <!--- What does this sample do? What problem does it solve? -->
    <!--- If it fixes/closes/resolves an open issue, please link to the
    issue here -->
    
    ## How Has This Been Tested?
    <!-- (if applicable) -->
    <!--- Please describe in detail how you tested your sample/changes. -->
    <!--- Include details of your testing environment, and the tests you ran
    to -->
    <!--- see how your change affects other areas of the code, etc. -->
    
    ## Screenshots
    <!-- (if appropriate): -->
    
    ## Types of changes
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    - [ ] Improvement (non-breaking change that adds a new feature)
    - [x] Bug fix (fixes an issue)
    - [ ] Breaking change (breaking change)
    - [ ] Documentation Improvement
    - [ ] Config and build (change in the configuration and build system,
    has no impact on code or features)
    
    ## Checklist:
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    - [ ] My code follows the code style of this project.
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    - [ ] I have added tests to cover my changes.
    - [ ] All new and existing tests passed.
    
    
    You agree with the license terms and that other license types may be
    granted with permission of the original `N64FlashcartMenu` project
    license holders.
    
    <!--- It would be nice if you could sign off your contribution by
    replacing the name with your GitHub user name and GitHub email contact.
    -->
    Signed-off-by: GITHUB_USER <GITHUB_USER_EMAIL>

--- src/boot/cheats.c
+++ src/boot/cheats.c
@@ -41,6 +41,9 @@ typedef struct {
 
 /** @brief Special cheat types enumeration */
 typedef enum {
+    SPECIAL_CLEAR_MEMORY = 0x20, /**< Clear memory between 0x80000200-0x80000300 on boot */
+    SPECIAL_SECONDARY_EXCEPTION_HANDLER = 0xCC, /**< Use alternate exception handler */
+    SPECIAL_SET_ENTRYPOINT_ADDR = 0xDE, /**< Set boot entrypoint address */
     SPECIAL_DISABLE_EXPANSION_PAK = 0xEE, /**< Disable Expansion Pak */
     SPECIAL_WRITE_BYTE_ON_BOOT = 0xF0, /**< Write byte on boot */
     SPECIAL_WRITE_SHORT_ON_BOOT = 0xF1, /**< Write short on boot */
@@ -56,6 +59,28 @@ typedef enum {
 #define IS_TYPE_CONDITIONAL(t) (((t)&0xF0) == 0xD0)
 
 #define IS_DOUBLE_ENTRY(t) (IS_TYPE_CONDITIONAL(t) || IS_TYPE_REPEATER(t))
+
+#define X106_XOR_CONSTANT (0x0260BCD5)
+#define X106_ENC_START (0x13C)
+
+/**
+ * @brief Get the XOR value for a given offset in the CIC x106 encrypted area.
+ *
+ * Calls to this function ought to always be reduced to constants.
+ *
+ * @param seed The IPL3 checksum seed (should always be 0x85 for x106; see cic_get_seed()).
+ * @param offset The offset in the encrypted area to calculate for.
+ * @return the calculated XOR value.
+ */
+__attribute__((always_inline))
+static inline uint32_t cheats_calc_x106_xor(uint8_t seed, uint8_t offset) {
+    uint32_t val = X106_XOR_CONSTANT * seed + 1;
+    #pragma GCC unroll 256
+    for (uint8_t i = 0; i < offset; i++) {
+        val *= X106_XOR_CONSTANT;
+    }
+    return val;
+}
 
 /**
  * @brief Patch the IPL3 with the cheat engine.
@@ -73,7 +98,7 @@ static bool cheats_patch_ipl3 (cic_type_t cic_type, io32_t *target) {
     switch (cic_type) {
     case CIC_5101: patch_offset = 476; break;
     case CIC_6101:
-    case CIC_7102: patch_offset = 476; break;
+    case CIC_7102: patch_offset = 466; break;
     case CIC_x102: patch_offset = 475; break;
     case CIC_x103: patch_offset = 472; break;
     case CIC_x105: patch_offset = 499; break;
@@ -84,7 +109,13 @@ static bool cheats_patch_ipl3 (cic_type_t cic_type, io32_t *target) {
     // NOTE: Check for "jr $t1" instruction
     //       Libdragon IPL3 could be brute-force signed with any retail
     //       CIC seed and checksum, and we support only retail libultra IPL3
-    if (cpu_io_read(&ipl3[patch_offset]) != I_JR(REG_T1)) {
+    uint32_t test_instruction = cpu_io_read(&ipl3[patch_offset]);
+    if (cic_type == CIC_x106) {
+        // NOTE: CIC x106 IPL3 is partially scrambled
+        test_instruction ^= cheats_calc_x106_xor(cic_get_seed(cic_type), patch_offset - X106_ENC_START);
+    }
+
+    if (test_instruction != I_JR(REG_T1)) {
         return false;
     }
 
@@ -96,7 +127,7 @@ static bool cheats_patch_ipl3 (cic_type_t cic_type, io32_t *target) {
 
     case CIC_x106:
         // NOTE: CIC x106 IPL3 is partially scrambled
-        j_instruction ^= 0x8188764A;
+        j_instruction ^= cheats_calc_x106_xor(cic_get_seed(cic_type), patch_offset - X106_ENC_START);
         break;
 
     default: break;
@@ -243,74 +274,91 @@ bool cheats_install (cic_type_t cic_type, uint32_t *cheat_list) {
     while (cheats_get_next(&cheat_list, &cheat)) {
         cheat_t *c = &cheat.main;
 
-        if (IS_TYPE_REPEATER(c->type)) {
-            if ((!IS_TYPE_WRITE(cheat.sub.type)) || IS_CONDITION_GS_BUTTON(cheat.sub.type)) {
-                continue;
-            }
-
-            int count = ((c->address >> 8) & 0xFF);
-            int step = (c->address & 0xFF);
-            int16_t increment = (int16_t)(c->value);
-
-            c = &cheat.sub;
-
-            for (int i = 0; i < count; i++) {
-                *engine_p++ = I_LUI(REG_K0, A_BASE(c->address));
-                *engine_p++ = I_ORI(REG_K1, REG_ZERO, c->value);
-                *engine_p++ = IS_WIDTH_16(c->type) ? I_SH(REG_K1, A_OFFSET(c->address), REG_K0)
-                                                   : I_SB(REG_K1, A_OFFSET(c->address), REG_K0);
-
-                c->address += step;
-                c->value += increment;
-            }
-
-            continue;
-        }
-
-        if (IS_TYPE_CONDITIONAL(c->type)) {
-            if ((!IS_TYPE_WRITE(cheat.sub.type)) || IS_CONDITION_GS_BUTTON(cheat.sub.type)) {
-                continue;
-            }
-
-            *engine_p++ = I_LUI(REG_K0, A_BASE(c->address));
-            *engine_p++ = IS_WIDTH_16(c->type) ? I_LHU(REG_K0, A_OFFSET(c->address), REG_K0)
-                                               : I_LBU(REG_K0, A_OFFSET(c->address), REG_K0);
-            *engine_p++ = I_ORI(REG_K1, REG_ZERO, c->value & (IS_WIDTH_16(c->type) ? 0xFFFF : 0xFF));
-            *engine_p++ = IS_CONDITION_NOT_EQUAL(c->type) ? I_BEQ(REG_K0, REG_K1, 3) : I_BNE(REG_K0, REG_K1, 3);
-
-            c = &cheat.sub;
-        }
-
-        if (IS_TYPE_WRITE(c->type)) {
-            if (IS_CONDITION_GS_BUTTON(c->type)) {
-                continue;
-            }
-
-            *engine_p++ = I_LUI(REG_K0, A_BASE(c->address));
-            *engine_p++ = I_ORI(REG_K1, REG_ZERO, c->value);
-            *engine_p++ = IS_WIDTH_16(c->type) ? I_SH(REG_K1, A_OFFSET(c->address), REG_K0)
-                                               : I_SB(REG_K1, A_OFFSET(c->address), REG_K0);
-
-            continue;
-        }
-
         switch (c->type) {
-        case SPECIAL_WRITE_BYTE_ON_BOOT:
-        case SPECIAL_WRITE_SHORT_ON_BOOT: {
-            *patcher_p++ = I_LUI(REG_K0, A_BASE(c->address));
-            *patcher_p++ = I_ORI(REG_K1, REG_ZERO, c->value);
-            *patcher_p++ = IS_WIDTH_16(c->type) ? I_SH(REG_K1, A_OFFSET(c->address), REG_K0)
-                                                : I_SB(REG_K1, A_OFFSET(c->address), REG_K0);
-            break;
-        }
-        case SPECIAL_DISABLE_EXPANSION_PAK: {
-            *patcher_p++ = I_LUI(REG_K0, 0xA000);
-            *patcher_p++ = I_LUI(REG_K1, 0x0040);
-            *patcher_p++ = I_SW(REG_K1, 0x318, REG_K0);
-            *patcher_p++ = I_SW(REG_K1, 0x3F0, REG_K0);
-            break;
-        }
-        default: break;
+            case SPECIAL_WRITE_BYTE_ON_BOOT:
+            case SPECIAL_WRITE_SHORT_ON_BOOT: {
+                *patcher_p++ = I_LUI(REG_K0, A_BASE(c->address));
+                *patcher_p++ = I_ORI(REG_K1, REG_ZERO, c->value);
+                *patcher_p++ = IS_WIDTH_16(c->type) ? I_SH(REG_K1, A_OFFSET(c->address), REG_K0)
+                                                    : I_SB(REG_K1, A_OFFSET(c->address), REG_K0);
+                break;
+            }
+            case SPECIAL_CLEAR_MEMORY: {
+                *patcher_p++ = I_LUI(REG_K0, 0xA000);
+                *patcher_p++ = I_ORI(REG_K1, REG_K0, (0x300 - 0x200) - 4);
+                *patcher_p++ = I_SW(REG_ZERO, 0x0200, REG_K0);
+                *patcher_p++ = I_BNE(REG_K0, REG_K1, -2); // could be BNEL
+                *patcher_p++ = I_ADDIU(REG_K0, REG_K0, 4);
+                break;
+            }
+            // N/A
+            case SPECIAL_SECONDARY_EXCEPTION_HANDLER:
+            // not needed with N64FlashcartMenu's boot method
+            case SPECIAL_SET_ENTRYPOINT_ADDR:
+            // already handled
+            case SPECIAL_SET_STORE_LOCATION: {
+                // do nothing
+                break;
+            }
+            case SPECIAL_DISABLE_EXPANSION_PAK: {
+                *patcher_p++ = I_LUI(REG_K0, 0xA000);
+                *patcher_p++ = I_LUI(REG_K1, 0x0040);
+                *patcher_p++ = I_SW(REG_K1, 0x318, REG_K0);
+                *patcher_p++ = I_SW(REG_K1, 0x3F0, REG_K0);
+                break;
+            }
+            default: {
+                if (IS_TYPE_REPEATER(c->type)) {
+                    if ((!IS_TYPE_WRITE(cheat.sub.type)) || IS_CONDITION_GS_BUTTON(cheat.sub.type)) {
+                        continue;
+                    }
+
+                    int count = ((c->address >> 8) & 0xFF);
+                    int step = (c->address & 0xFF);
+                    int16_t increment = (int16_t)(c->value);
+
+                    c = &cheat.sub;
+
+                    for (int i = 0; i < count; i++) {
+                        *engine_p++ = I_LUI(REG_K0, A_BASE(c->address));
+                        *engine_p++ = I_ORI(REG_K1, REG_ZERO, c->value);
+                        *engine_p++ = IS_WIDTH_16(c->type) ? I_SH(REG_K1, A_OFFSET(c->address), REG_K0)
+                                                        : I_SB(REG_K1, A_OFFSET(c->address), REG_K0);
+
+                        c->address += step;
+                        c->value += increment;
+                    }
+
+                    continue;
+                }
+
+                if (IS_TYPE_CONDITIONAL(c->type)) {
+                    if ((!IS_TYPE_WRITE(cheat.sub.type)) || IS_CONDITION_GS_BUTTON(cheat.sub.type)) {
+                        continue;
+                    }
+
+                    *engine_p++ = I_LUI(REG_K0, A_BASE(c->address));
+                    *engine_p++ = IS_WIDTH_16(c->type) ? I_LHU(REG_K0, A_OFFSET(c->address), REG_K0)
+                                                    : I_LBU(REG_K0, A_OFFSET(c->address), REG_K0);
+                    *engine_p++ = I_ORI(REG_K1, REG_ZERO, c->value & (IS_WIDTH_16(c->type) ? 0xFFFF : 0xFF));
+                    *engine_p++ = IS_CONDITION_NOT_EQUAL(c->type) ? I_BEQ(REG_K0, REG_K1, 3) : I_BNE(REG_K0, REG_K1, 3);
+
+                    c = &cheat.sub;
+                }
+
+                if (IS_TYPE_WRITE(c->type)) {
+                    if (IS_CONDITION_GS_BUTTON(c->type)) {
+                        continue;
+                    }
+
+                    *engine_p++ = I_LUI(REG_K0, A_BASE(c->address));
+                    *engine_p++ = I_ORI(REG_K1, REG_ZERO, c->value);
+                    *engine_p++ = IS_WIDTH_16(c->type) ? I_SH(REG_K1, A_OFFSET(c->address), REG_K0)
+                                                    : I_SB(REG_K1, A_OFFSET(c->address), REG_K0);
+
+                    continue;
+                }
+            }
         }
     }
 

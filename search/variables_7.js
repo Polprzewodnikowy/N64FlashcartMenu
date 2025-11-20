c22078981b9b97fd22b91a74eaa4b0532a3c7268
networkfusion <networkfusion@users.noreply.github.com>
2025-11-20 00:28:48 +0000

    deploy: 33995e4b7191e6c7f7be6653e379c1c3903ae527

--- search/variables_7.js
+++ search/variables_7.js
@@ -2,6 +2,5 @@ var searchData=
 [
   ['game_5fcode_0',['game_code',['../rom__info_8h.html#a4f17ea269979c808050cd0de5aa32afa',1,'rom_info_t.__unnamed22__']]],
   ['gamecode_1',['gamecode',['../cpakfs__utils_8h.html#ae9da9578e7b8ae989a881e322a82ceed',1,'cpakfs_path_strings_t']]],
-  ['get_5fdefault_5fselection_2',['get_default_selection',['../ui__components_8h.html#a892a6833e57f25fe230c2cf1623c0fa0',1,'component_context_menu_t']]],
-  ['get_5ffirmware_5fversion_3',['get_firmware_version',['../flashcart_8h.html#af5b584e89ac94844a234ef9ae56dfff3',1,'flashcart_t']]]
+  ['get_5ffirmware_5fversion_2',['get_firmware_version',['../flashcart_8h.html#af5b584e89ac94844a234ef9ae56dfff3',1,'flashcart_t']]]
 ];

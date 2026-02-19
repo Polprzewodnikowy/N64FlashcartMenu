61ee4a5fa6c93ba5dd555f6c94a792c265154005
Robin Jones <networkfusion@users.noreply.github.com>
2026-02-19 20:43:45 +0000

    Add required changes
    For some reason we are getting crashes. to revisit.

--- src/menu/sound.h
+++ src/menu/sound.h
@@ -9,8 +9,9 @@
 
 #include <stdbool.h>
 
-#define SOUND_SFX_CHANNEL           (0) /**< Channel for sound effects */
-#define SOUND_MP3_PLAYER_CHANNEL    (2) /**< Channel for MP3 player sound */
+#define SOUND_SFX_CHANNEL           (0) /**< First Channel of sound effects 0-7 (8 in total) */
+#define SOUND_BGM_CHANNEL           (8) /**< First Channel for background music 8-9 (2 in total [stereo]) */
+#define SOUND_MP3_PLAYER_CHANNEL    (10) /**< First Channel for MP3 player sound 10-15 (6 in total [surround sound possible]) */
 
 
 /**
@@ -59,11 +60,34 @@ void sound_init_sfx(void);
 void sound_use_sfx(bool enable);
 
 /**
+ * @brief Initialize the background music system.
+ * 
+ * This function initializes the background music system, setting up
+ * necessary resources and configurations for playing background music.
+ */
+void sound_init_bgm(void);
+
+
+/**
+ * @brief Enable or disable background music.
+ * 
+ * @param enable True to enable background music, false to disable.
+ */
+void sound_use_bgm(bool enable);
+
+/**
  * @brief Play a specified sound effect.
  * 
  * @param sfx The sound effect to play, as defined in sound_effect_t.
  */
 void sound_play_effect(sound_effect_t sfx);
+
+/**
+ * @brief Play background music.
+ * 
+ * This function starts playing the background music if it is enabled.
+ */
+void sound_play_bgm(void);
 
 /**
  * @brief Deinitialize the sound system.

5ac3f3439d33cb25bf04b0bc575b0d4ffb3012ce
Robin Jones <networkfusion@users.noreply.github.com>
2026-03-24 22:23:11 +0000

    fix for patch enable setting

--- src/menu/rom_info.c
+++ src/menu/rom_info.c
@@ -1205,7 +1205,7 @@ rom_err_t rom_config_setting_set_cheats (path_t *path, rom_info_t *rom_info, boo
 #ifdef FEATURE_PATCHER_GUI_ENABLED
 rom_err_t rom_config_setting_set_patches (path_t *path, rom_info_t *rom_info, bool enabled) {
     rom_info->settings.patches_enabled = enabled;
-    return save_rom_config_setting_to_file(path, NULL, "patches_enabled", enabled, false);
+    return save_rom_config_setting_to_file(path, "general", "patches_enabled", enabled, false);
 }
 #endif
 

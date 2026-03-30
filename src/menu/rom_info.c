573aa0329f73af406de63536f6aab4412d1365d3
Robin Jones <networkfusion@users.noreply.github.com>
2026-03-30 19:09:26 +0100

    Further use of not specified, rather than unknown.

--- src/menu/rom_info.c
+++ src/menu/rom_info.c
@@ -763,10 +763,10 @@ static void extract_rom_info (match_t *match, rom_header_t *rom_header, rom_info
     }
 
     rom_info->meta.name = strdup("");
-    rom_info->meta.author = strdup("Unknown");
-    rom_info->meta.release_date = strdup("Unknown");
+    rom_info->meta.author = strdup("Not specified");
+    rom_info->meta.release_date = strdup("Not specified");
     rom_info->meta.osi_license = strdup("Not specified");
-    rom_info->meta.website = strdup("Unknown");
+    rom_info->meta.website = strdup("Not specified");
     rom_info->meta.age_rating = 0;
     rom_info->meta.short_description = strdup("");
 
@@ -848,7 +848,7 @@ static bool load_metadata_from_zip_file (const char *zip_path, rom_info_t *rom_i
         free(rom_info->meta.author);
         rom_info->meta.author = strdup(ini_get_string(meta_ini, "meta", "author", "Not specified"));
         free(rom_info->meta.release_date);
-        rom_info->meta.release_date = strdup(ini_get_string(meta_ini, "meta", "release-date", "Unknown"));
+        rom_info->meta.release_date = strdup(ini_get_string(meta_ini, "meta", "release-date", "Not specified"));
         free(rom_info->meta.osi_license);
         rom_info->meta.osi_license = strdup(ini_get_string(meta_ini, "meta", "osi-license", "Not specified"));
         free(rom_info->meta.website);
@@ -972,7 +972,7 @@ static bool load_rom_meta_from_embedded_zip (const char *rom_path, rom_header_t 
         free(rom_info->meta.author);
         rom_info->meta.author = strdup(ini_get_string(meta_ini, "meta", "author", "Not specified"));
         free(rom_info->meta.release_date);
-        rom_info->meta.release_date = strdup(ini_get_string(meta_ini, "meta", "release-date", "Unknown"));
+        rom_info->meta.release_date = strdup(ini_get_string(meta_ini, "meta", "release-date", "Not specified"));
         free(rom_info->meta.osi_license);
         rom_info->meta.osi_license = strdup(ini_get_string(meta_ini, "meta", "osi-license", "Not specified"));
         free(rom_info->meta.website);
@@ -1039,7 +1039,7 @@ static void load_rom_meta_from_file (path_t *path, rom_info_t *rom_info) {
         free(rom_info->meta.author);
         rom_info->meta.author = strdup(ini_get_string(rom_meta_ini, "meta", "author", "Not specified"));
         free(rom_info->meta.release_date);
-        rom_info->meta.release_date = strdup(ini_get_string(rom_meta_ini, "meta", "release-date", "Unknown"));
+        rom_info->meta.release_date = strdup(ini_get_string(rom_meta_ini, "meta", "release-date", "Not specified"));
         free(rom_info->meta.osi_license);
         rom_info->meta.osi_license = strdup(ini_get_string(rom_meta_ini, "meta", "osi-license", "Not specified"));
         free(rom_info->meta.website);

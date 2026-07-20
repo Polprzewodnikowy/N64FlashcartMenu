b40e929a815134e5fc89045f12c507a5986ced17
Suprapote <111246491+Suprapote@users.noreply.github.com>
2026-07-20 19:21:24 +0200

    Update settings_editor.c

--- src/menu/views/settings_editor.c
+++ src/menu/views/settings_editor.c
@@ -595,7 +595,7 @@ static void draw (menu_t *menu, surface_t *d) {
             ALIGN_LEFT, VALIGN_BOTTOM,
             "* NOTE: This setting may cause the display to go dark. If that happens, wait 10 seconds or press B or manually edit the menu/config.ini on the SD card.\n"
         );
-    } else {
+    } else if (selected_tab == SETTINGS_TAB_VIDEO && get_tv_type() != TV_PAL) {
         ui_components_main_text_draw(
             STL_RED,
             ALIGN_LEFT, VALIGN_BOTTOM,

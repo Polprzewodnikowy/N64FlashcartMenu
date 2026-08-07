e001c9fe40949970d8bfd21847246668ebb5588d
Robin Jones <networkfusion@users.noreply.github.com>
2026-08-07 12:36:37 +0100

    Initialize clear_rdram to false in all non-ROM boot paths
    Disk, emulator, and USB-reboot paths were not setting boot_params->
    clear_rdram, leaving it uninitialised.  Default to false in all
    paths that do not explicitly request RDRAM clearing.

--- src/menu/views/load_disk.c
+++ src/menu/views/load_disk.c
@@ -286,11 +286,13 @@ static void load (menu_t *menu) {
             default: menu->boot_params->tv_type = BOOT_TV_TYPE_PASSTHROUGH; break;
         }
         menu->boot_params->cheat_list = NULL;
+        menu->boot_params->clear_rdram = false;
     } else {
         menu->boot_params->device_type = BOOT_DEVICE_TYPE_64DD;
         menu->boot_params->tv_type = BOOT_TV_TYPE_NTSC;
         menu->boot_params->detect_cic_seed = true;
         menu->boot_params->cheat_list = NULL;
+        menu->boot_params->clear_rdram = false;
     }
 }
 
